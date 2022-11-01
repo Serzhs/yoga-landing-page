e02379ce19e78c083b414c4c06c8526676746ec9
Serzsh <serzants.janis@gmail.com>
2022-11-01 14:44:35 +0200

    Added better docs

--- scripts.js
+++ scripts.js
@@ -10,15 +10,21 @@ let normalModeOn = true
 logoEl.addEventListener('click', () => {
     // check if normal mode
     if(normalModeOn === true) {
+        // change --blue-accent value
         rootEl.style.setProperty('--blue-accent', 'red');
+        // change --white-accent value
         rootEl.style.setProperty('--white-accent', '#000');
+        // change --black-accent value
         rootEl.style.setProperty('--black-accent', '#fff');
 
         // now it is not normal any more :D 
         normalModeOn = false
     } else {
+         // change --blue-accent value
         rootEl.style.setProperty('--blue-accent', '#2f6ee2');
+         // change --white-accent value
         rootEl.style.setProperty('--white-accent', '#fff');
+         // change --black-accent value
         rootEl.style.setProperty('--black-accent', '#000');
 
         // normal again :D 
